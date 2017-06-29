2be1d180d5927aa982944324d9618756f2e4db34
Tomáš Votruba <info@tomasvotruba.cz>
2017-06-29 18:07:14 +0200

    [Statie] add amp (#201)
    * composer: add amp library, packages were sorted
    
    * [Statie] add simple HtmlToAmpConvertor, add to RenderableFilesProcessor
    
    * [Statie] add mutual amp links
    
    * [Statie] allow config based configuration + make amp configurable
    
    * [Statie] use ConfigFilePathHelper, misc, use ParameterProvider from PackageBuilder
    
    * [PackageBuilder] move ParmaeterProvider test here
    
    * fixes
    
    * fix name
    
    * fix cs

---
+++ src/Adapter/Symfony/Parameter/ParameterProvider.php
@@ -0,0 +1,49 @@
+<?php declare(strict_types=1);
+
+namespace Symplify\PackageBuilder\Adapter\Symfony\Parameter;
+
+use Nette\Utils\Strings;
+use Symfony\Component\DependencyInjection\Container;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+final class ParameterProvider
+{
+    /**
+     * @var mixed[]
+     */
+    private $parameters = [];
+
+    /**
+     * @param Container|ContainerInterface $container
+     */
+    public function __construct(ContainerInterface $container)
+    {
+        $parameters = $container->getParameterBag()
+            ->all();
+
+        $this->parameters = $this->unsetKernelParameters($parameters);
+    }
+
+    /**
+     * @return mixed[]
+     */
+    public function provide(): array
+    {
+        return $this->parameters;
+    }
+
+    /**
+     * @param mixed[] $parameters
+     * @return mixed[]
+     */
+    private function unsetKernelParameters(array $parameters): array
+    {
+        foreach ($parameters as $name => $value) {
+            if (Strings::startsWith($name, 'kernel')) {
+                unset($parameters[$name]);
+            }
+        }
+
+        return $parameters;
+    }
+}
