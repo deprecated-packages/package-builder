d40c46839482820c77969e26281ccb917ca3c19b
TomasVotruba <tomas.vot@gmail.com>
2020-01-27 22:55:20 +0100

    try to fix git push

---
+++ tests/Parameter/ParameterProviderTest.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\PackageBuilder\Tests\Parameter;
+
+use Symplify\PackageBuilder\Parameter\ParameterProvider;
+use Symplify\PackageBuilder\Tests\AbstractKernelTestCase;
+use Symplify\PackageBuilder\Tests\HttpKernel\PackageBuilderTestKernel;
+
+final class ParameterProviderTest extends AbstractKernelTestCase
+{
+    public function test(): void
+    {
+        $this->bootKernelWithConfigs(
+            PackageBuilderTestKernel::class,
+            [__DIR__ . '/ParameterProviderSource/config.yml']
+        );
+
+        $parameterProvider = self::$container->get(ParameterProvider::class);
+        $this->assertContains([
+            'key' => 'value',
+            'camelCase' => 'Lion',
+            'pascal_case' => 'Celsius',
+        ], $parameterProvider->provide());
+
+        $this->assertSame('value', $parameterProvider->provideParameter('key'));
+
+        $parameterProvider->changeParameter('key', 'anotherKey');
+        $this->assertSame('anotherKey', $parameterProvider->provideParameter('key'));
+    }
+
+    public function testIncludingYaml(): void
+    {
+        $this->bootKernelWithConfigs(
+            PackageBuilderTestKernel::class,
+            [__DIR__ . '/ParameterProviderSource/Yaml/including-config.yml']
+        );
+
+        $parameterProvider = self::$container->get(ParameterProvider::class);
+
+        $this->assertContains([
+            'one' => 1,
+            'two' => 2,
+        ], $parameterProvider->provide());
+
+        $this->assertArrayHasKey('kernel.project_dir', $parameterProvider->provide());
+    }
+}
