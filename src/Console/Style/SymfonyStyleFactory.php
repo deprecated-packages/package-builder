f0893078580fd5a06882c28ee29a82a8f5d1556f
TomasVotruba <tomas.vot@gmail.com>
2020-02-18 17:14:51 +0100

    remove dead commands

---
+++ src/Console/Style/SymfonyStyleFactory.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\PackageBuilder\Console\Style;
+
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Input\ArgvInput;
+use Symfony\Component\Console\Output\ConsoleOutput;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symplify\PackageBuilder\Reflection\PrivatesCaller;
+
+final class SymfonyStyleFactory
+{
+    /**
+     * @var PrivatesCaller
+     */
+    private $privatesCaller;
+
+    public function __construct()
+    {
+        $this->privatesCaller = new PrivatesCaller();
+    }
+
+    public function create(): SymfonyStyle
+    {
+        // to prevent missing argv indes
+        if (! isset($_SERVER['argv'])) {
+            $_SERVER['argv'] = [];
+        }
+
+        $input = new ArgvInput();
+        $output = new ConsoleOutput();
+
+        // to configure all -v, -vv, -vvv options without memory-lock to Application run() arguments
+        $this->privatesCaller->callPrivateMethod(new Application(), 'configureIO', $input, $output);
+
+        // --debug is called
+        if ($input->hasParameterOption('--debug')) {
+            $output->setVerbosity(OutputInterface::VERBOSITY_DEBUG);
+        }
+
+        return new SymfonyStyle($input, $output);
+    }
+}
