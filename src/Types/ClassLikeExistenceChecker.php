a827c67b340d31c15fb91775230a32f7f3616fcd
TomasVotruba <tomas.vot@gmail.com>
2020-02-19 14:36:47 +0100

    move rector paths to config

---
+++ src/Types/ClassLikeExistenceChecker.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\PackageBuilder\Types;
+
+final class ClassLikeExistenceChecker
+{
+    public function exists(string $type): bool
+    {
+        return class_exists($type) || interface_exists($type) || trait_exists($type);
+    }
+}
