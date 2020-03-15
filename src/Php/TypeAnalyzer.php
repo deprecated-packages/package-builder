197a3dc459ff853c7bdadf8d3a30497214e8ff69
TomasVotruba <tomas.vot@gmail.com>
2020-03-15 01:07:04 +0100

    decouple SymfonyStaticDumperConfiguratoin

---
+++ src/Php/TypeAnalyzer.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\PackageBuilder\Php;
+
+use Nette\Utils\Strings;
+
+final class TypeAnalyzer
+{
+    public function isPhpReservedType(string $type): bool
+    {
+        return in_array(
+            $type,
+            [
+                'string',
+                'bool',
+                'mixed',
+                'object',
+                'iterable',
+                'resource',
+                'array',
+                'float',
+                'int',
+                'boolean',
+                'integer',
+                'double',
+                'null',
+                'false',
+                'true',
+                'mixed',
+            ],
+            true
+        );
+    }
+
+    public function isIterableType(string $type): bool
+    {
+        return Strings::endsWith($type, '[]');
+    }
+}
