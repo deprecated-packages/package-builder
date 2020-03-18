b0122da9bb88408461ed7e3acca720180a23ef2e
TomasVotruba <tomas.vot@gmail.com>
2020-03-18 14:38:08 +0100

    [ECS] Fix missing autoload in ECS Tester

---
+++ src/Console/ShellCode.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\PackageBuilder\Console;
+
+final class ShellCode
+{
+    /**
+     * @var int
+     */
+    public const SUCCESS = 0;
+
+    /**
+     * @var int
+     */
+    public const ERROR = 1;
+}
