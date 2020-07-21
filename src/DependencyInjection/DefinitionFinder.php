5cc3ec0455fe8dbcd111fb0aa42c6853d5877e81
TomasVotruba <tomas.vot@gmail.com>
2020-07-21 14:32:12 +0200

    sonar: add easy testing

---
+++ src/DependencyInjection/DefinitionFinder.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\PackageBuilder\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
+use Symplify\PackageBuilder\Exception\DependencyInjection\DefinitionForTypeNotFoundException;
+
+final class DefinitionFinder
+{
+    /**
+     * @return Definition[]
+     */
+    public function findAllByType(ContainerBuilder $containerBuilder, string $type): array
+    {
+        $definitions = [];
+        foreach ($containerBuilder->getDefinitions() as $name => $definition) {
+            $class = $definition->getClass() ?: $name;
+            if (! is_string($class)) {
+                continue;
+            }
+
+            if (is_a($class, $type, true)) {
+                $definitions[$name] = $definition;
+            }
+        }
+
+        return $definitions;
+    }
+
+    public function getByType(ContainerBuilder $containerBuilder, string $type): Definition
+    {
+        $definition = self::getByTypeIfExists($containerBuilder, $type);
+        if ($definition !== null) {
+            return $definition;
+        }
+
+        throw new DefinitionForTypeNotFoundException(sprintf('Definition for type "%s" was not found.', $type));
+    }
+
+    public function getByTypeIfExists(ContainerBuilder $containerBuilder, string $type): ?Definition
+    {
+        foreach ($containerBuilder->getDefinitions() as $name => $definition) {
+            $class = $definition->getClass() ?: $name;
+            if (! is_string($class)) {
+                continue;
+            }
+
+            if (is_a($class, $type, true)) {
+                return $definition;
+            }
+        }
+
+        return null;
+    }
+}
