5b5b2236879d2ef3524a4e87726dcae06815d09b
TomasVotruba <tomas.vot@gmail.com>
2020-08-12 14:06:30 +0200

    [ParameterNameGuard] Deprecated for PHP config with constants

---
+++ tests/Parameter/ParameterProviderTest.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\PackageBuilder\Tests\Parameter;
+
+use Symplify\PackageBuilder\Parameter\ParameterProvider;
+use Symplify\PackageBuilder\Tests\AbstractKernelTestCase;
+use Symplify\PackageBuilder\Tests\HttpKernel\PackageBuilderTestKernel;
+
+final class ParameterProviderTest extends AbstractKernelTestCase
+{
+    public function test(): void
+    {
+        $this->bootKernelWithConfigs(
+            PackageBuilderTestKernel::class,
+            [__DIR__ . '/ParameterProviderSource/config.yml']
+        );
+
+        $parameterProvider = self::$container->get(ParameterProvider::class);
+
+        $parameters = $parameterProvider->provide();
+        $this->assertArrayHasKey('key', $parameters);
+        $this->assertArrayHasKey('camelCase', $parameters);
+        $this->assertArrayHasKey('pascal_case', $parameters);
+
+        $this->assertSame('value', $parameters['key']);
+        $this->assertSame('Lion', $parameters['camelCase']);
+        $this->assertSame('Celsius', $parameters['pascal_case']);
+
+        $this->assertSame('value', $parameterProvider->provideParameter('key'));
+
+        $parameterProvider->changeParameter('key', 'anotherKey');
+        $this->assertSame('anotherKey', $parameterProvider->provideParameter('key'));
+    }
+
+    public function testIncludingYaml(): void
+    {
+        $this->bootKernelWithConfigs(
+            PackageBuilderTestKernel::class,
+            [__DIR__ . '/ParameterProviderSource/Yaml/including-config.yml']
+        );
+
+        $parameterProvider = self::$container->get(ParameterProvider::class);
+
+        $parameters = $parameterProvider->provide();
+        $this->assertArrayHasKey('one', $parameters);
+        $this->assertArrayHasKey('two', $parameters);
+
+        $this->assertSame(1, $parameters['one']);
+        $this->assertSame(2, $parameters['two']);
+
+        $this->assertArrayHasKey('kernel.project_dir', $parameterProvider->provide());
+    }
+}
