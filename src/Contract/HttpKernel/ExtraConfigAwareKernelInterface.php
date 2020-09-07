1bb5282b2cbef835d2f42bd8ec6e8a242bf88ae7
TomasVotruba <tomas.vot@gmail.com>
2020-09-07 16:56:51 +0200

    prepare release

---
+++ src/Contract/HttpKernel/ExtraConfigAwareKernelInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\PackageBuilder\Contract\HttpKernel;
+
+interface ExtraConfigAwareKernelInterface
+{
+    /**
+     * @param string[] $configs
+     */
+    public function setConfigs(array $configs): void;
+}
