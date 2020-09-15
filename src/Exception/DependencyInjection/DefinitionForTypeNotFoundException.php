2a72b00b7bbb5abd0e1af48ab2ceeed738ca4234
TomasVotruba <tomas.vot@gmail.com>
2020-09-15 13:59:27 +0200

    open 8.4-dev

---
+++ src/Exception/DependencyInjection/DefinitionForTypeNotFoundException.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\PackageBuilder\Exception\DependencyInjection;
+
+use Exception;
+
+final class DefinitionForTypeNotFoundException extends Exception
+{
+}
