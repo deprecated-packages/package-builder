e18a5fe261d6985424bcd8516443de166e681319
Tomas Votruba <tomas.vot@gmail.com>
2020-10-02 19:59:01 +0200

    various fixes (#2299)
    * static fixes
    
    * [CS] Skip interesection in ContainsTypeAnalyser
    
    * misc
    
    * allow method call in dim fethc
    
    * cleanup ctor static

---
+++ src/Matcher/ArrayStringAndFnMatcher.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\PackageBuilder\Matcher;
+
+final class ArrayStringAndFnMatcher
+{
+    /**
+     * @param string[] $matchingValues
+     */
+    public function isMatch(string $currenctValue, array $matchingValues): bool
+    {
+        foreach ($matchingValues as $matchingValue) {
+            if ($currenctValue === $matchingValue) {
+                return true;
+            }
+
+            if (fnmatch($matchingValue, $currenctValue)) {
+                return true;
+            }
+
+            if (fnmatch($matchingValue, $currenctValue, FNM_NOESCAPE)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * @param string[] $matchingValues
+     */
+    public function isMatchOrSubType(string $currenctValue, array $matchingValues): bool
+    {
+        if ($this->isMatch($currenctValue, $matchingValues)) {
+            return true;
+        }
+
+        foreach ($matchingValues as $matchingValue) {
+            if (is_a($currenctValue, $matchingValue, true)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
