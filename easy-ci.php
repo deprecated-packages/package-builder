7593f0a41a2b272583c5611203bfd33d56820cdd
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-02-05 20:06:02 +0700

    fix easy-ci

--- easy-ci.php
+++ easy-ci.php
@@ -2,15 +2,11 @@
 
 declare(strict_types=1);
 
+use Symplify\PackageBuilder\DependencyInjection\CompilerPass\AutowireInterfacesCompilerPass;
 use Symplify\EasyCI\Config\EasyCIConfig;
-use Symplify\PHPStanExtensions\TypeExtension\FuncCall\NativeFunctionDynamicFunctionReturnTypeExtension;
-use Symplify\PHPStanExtensions\TypeExtension\MethodCall\ContainerGetReturnTypeExtension;
-use Symplify\PHPStanExtensions\TypeExtension\MethodCall\SplFileInfoTolerantReturnTypeExtension;
 
 return static function (EasyCIConfig $easyCIConfig): void {
     $easyCIConfig->typesToSkip([
-        NativeFunctionDynamicFunctionReturnTypeExtension::class,
-        ContainerGetReturnTypeExtension::class,
-        SplFileInfoTolerantReturnTypeExtension::class,
+        AutowireInterfacesCompilerPass::class,
     ]);
 };
